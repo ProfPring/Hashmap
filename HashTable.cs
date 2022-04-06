020e177e8ab0ec3b75bb7700ccc26bcb1192bc97
unknown <samrobbo1997@live.co.uk>
2022-04-06 16:54:23 +0100

    first commit

---
+++ HashTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace HashMap
+{
+    public class HashTable<Tkey, TValue>
+    {
+        private KeyValuePair<Tkey, TValue>[] KeyValuePairArray;
+        public HashTable(int size)
+        {
+            KeyValuePairArray = new KeyValuePair<Tkey, TValue>[size];
+        }
+
+        private int  HashFunction(Tkey key, TValue value)  
+        {
+            string unnHashedValue = $"{value.ToString()}";
+            byte[] ASCIIvalues = Encoding.ASCII.GetBytes(unnHashedValue);
+
+            //add acsii codes togther and divide by the ammount of elements in the array
+            int sum = 0;
+            foreach(byte ASCIIvalue in ASCIIvalues)
+            {
+                sum += ASCIIvalue;
+            }
+            int Result;
+            int hashedValue = Math.DivRem( sum, KeyValuePairArray.Length , out Result);
+            return Result;
+        }
+        public void Add(Tkey key, TValue value)
+        {
+            int hasCode = HashFunction(key, value);
+            KeyValuePairArray[hasCode] = new KeyValuePair<Tkey, TValue>(key, value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashMap
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            HashTable<int, string> myHashmap = new HashTable<int, string>(10);
+
+            myHashmap.Add(0, "Mia");
+        }
+    }
+}
